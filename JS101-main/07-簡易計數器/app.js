564eee3593ea140cd4ea9c584b7992a2dae1690e
shelly <shelly808002121231@gmail.com>
2023-06-22 08:45:06 +0800

    add JS101 online class Quizes files

---
+++ JS101-main/07-簡易計數器/app.js
@@ -0,0 +1,30 @@
+// 程式碼寫在這裡
+
+document.addEventListener("DOMContentLoaded" , () => { 
+    const plusBtn  = document.querySelector("#plus")
+    const minusBtn = document.querySelector("#minus")
+    const counter  = document.querySelector("#counter")
+
+
+    plusBtn.addEventListener("click" , () => {
+        let v = Number(counter.value)
+        v = v + 1
+        counter.value = v
+        
+        console.log("+")
+    })
+
+    minusBtn.addEventListener("click" , () => {
+        let v = Number(counter.value)
+        if ( v > 0){
+            v = v - 1
+         }
+        
+        counter.value = v
+              
+        console.log( "-" )
+    })
+
+ 
+
+} )
