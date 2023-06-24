12244a3f55b60ada80b8e8faf335cef5a106317b
shelly <shelly808002121231@gmail.com>
2023-06-24 10:34:34 +0800

    add comment for sugar2's answer

--- 糖果題/sugar.js
+++ 糖果題/sugar.js
@@ -8,32 +8,27 @@ const chars2 = ["O", "Q", "R", "S"]
 
 function missingChar(chars) {
   const newArray = chars.map((e) => e.charCodeAt());
+ 
   
   const firstE = newArray[0];
   const lastE = newArray[newArray.length - 1];
   const afterChar = [];
-
   for (let j = firstE; j <= lastE; j++) {
     let newChar = String.fromCharCode(j);
     afterChar.push(newChar);
-  }
-
+    }
   const missingChar = afterChar.filter((char) => !chars.includes(char));
-
   return missingChar;
-
-  
+  //  使用charCodeAr將變數轉換成Unicode 编码，然後定義一個迴圈，創造出沒有缺少字元的陣列，將兩個陣列使用fliter進行比對，結合includes去交叉比對後，得到缺少的字元
   
 }
-
-
-
 
 console.log(missingChar(chars1)) // 印出 e
 console.log(missingChar(chars2)) // 印出 P
 
 // 提示：
 // 可使用字串的 charCodeAt 方法...
+
 
 
 
