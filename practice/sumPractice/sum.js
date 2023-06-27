2625205b91dca01a2ba716e90fffce09b4de7f2c
shelly <shelly808002121231@gmail.com>
2023-06-27 22:56:25 +0800

    Put all the sum files into new floder in order to practice

---
+++ practice/sumPractice/sum.js
@@ -0,0 +1,19 @@
+// list => [1, 4, 2, 3, 8, 7, 10, 12]
+// 挑出陣列裡的奇數，乘以 3 之後，計算總和
+
+// for loop 
+
+// let list =  [1, 4, 2, 3, 8, 7, 10, 12]
+// let sum= 0
+// for( i = 0 ; i < list.length ; i++ ){
+  
+//   if(list[i]% 2 !== 0 ){
+//     sum += list[i]*3
+//     // sum = sum + list[i]
+
+//   }
+
+// }
+// console.log( sum )
+
+
