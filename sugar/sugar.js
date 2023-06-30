c09538f7c4055aba5f254773e2cc912d01c80e7f
shelly <shelly808002121231@gmail.com>
2023-07-01 00:48:49 +0800

    Try&errer:For Quiz_sugar06_To find what's the rootcasue of undefined

--- sugar/sugar.js
+++ sugar/sugar.js
@@ -2,10 +2,22 @@
 // 程式語言：JavaScript
 // 題目：找出在數字陣列裡跟其它元素不一樣的值
 
+function findDifferent(numbers, excludedValue) {
+  const uniqueElements = numbers.filter((e, index) => {
+    return numbers.indexOf(e) === index && e !== excludedValue;
+  });
+  return uniqueElements;
+}
+
+console.log(findDifferent([1, 1, 1, 1, 3, 1, 1, 1], 1)); // 印出 3
+//  console.log(findDifferent([2, 2, 2, 4, 2, 2])) // 印出 4
+//  console.log(findDifferent([8, 3, 3, 3, 3, 3, 3, 3])) // 印出 8
+
+
 // function findDifferent(numbers) {
 
 //   const uniqueElements = numbers.filter((e, index) => {
-//    return  numbers.indexOf(e) === index && e !== 1
+//    numbers.indexOf(e) === index && e !== 1
 
 //   });
 
@@ -13,18 +25,8 @@
 
 //  }
  
- console.log(findDifferent([1, 1, 1, 1, 3, 1, 1, 1])) // 印出 3
-//  console.log(findDifferent([2, 2, 2, 4, 2, 2])) // 印出 4
-//  console.log(findDifferent([8, 3, 3, 3, 3, 3, 3, 3])) // 印出 8
+//  console.log(findDifferent([1, 1, 1, 1, 3, 1, 1, 1]))
 
-function findDifferent(numbers) {
-  const uniqueElements = numbers.filter((element, index) => {
-    return numbers.indexOf(element) === index && element !== 1;
-  });
-  
-  return uniqueElements;
-}
 
-console.log(findDifferent([1, 1, 1, 1, 3, 1, 1, 1]));
 
 
