3e1b76b178b21c04d6850bbcd0b0ffdd2d8fbd3e
shelly <shelly808002121231@gmail.com>
2023-07-01 13:34:47 +0800

    Try&errer:_Quiz sugar06_how to use object [ ]

--- sugar/sugar.js
+++ sugar/sugar.js
@@ -3,30 +3,16 @@
 // 題目：找出在數字陣列裡跟其它元素不一樣的值
 
 function findDifferent(numbers, excludedValue) {
-  const uniqueElements = numbers.filter((e, index) => {
-    return numbers.indexOf(e) === index && e !== excludedValue;
-  });
-  return uniqueElements;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] !== excludedValue) {
+      return excludedValue;
+    }
+  }
+  return null; // 如果找不到不同的數字，可以返回null或其他適當的值
 }
 
-console.log(findDifferent([1, 1, 1, 1, 3, 1, 1, 1], 1)); // 印出 3
+console.log(findDifferent([1, 1, 1, 1, 3, 1, 1, 1])); // 印出 3
 //  console.log(findDifferent([2, 2, 2, 4, 2, 2])) // 印出 4
 //  console.log(findDifferent([8, 3, 3, 3, 3, 3, 3, 3])) // 印出 8
-
-
-// function findDifferent(numbers) {
-
-//   const uniqueElements = numbers.filter((e, index) => {
-//    numbers.indexOf(e) === index && e !== 1
-
-//   });
-
-
-
-//  }
- 
-//  console.log(findDifferent([1, 1, 1, 1, 3, 1, 1, 1]))
-
-
 
 
