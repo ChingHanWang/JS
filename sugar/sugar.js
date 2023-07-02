0f85c8d5807dbadcf03ab3dc33ef35cb2ed74efd
shelly <shelly808002121231@gmail.com>
2023-07-03 01:01:40 +0800

    Sugar06 :To name for variables of [i] and [j]

--- sugar/sugar.js
+++ sugar/sugar.js
@@ -4,42 +4,32 @@
 
 function findDifferent(numbers) {
   for (let i = 0; i < numbers.length; i++ ) { 
-    let  counter = 0
+    let counter = 0  
+    let indexNumber = numbers[i]
+    // console.log( "第" + i + "i=" + indexNumber )
     
-    
-          let a = numbers[i]
-          console.log( "第" + i + "i=" + a )
-
-
-          for (let j = 0; j < numbers.length; j++ ){
-            b = numbers[j]
-
-            console.log( "第" + j + "j=" + b)
-            
-          
-            if (numbers[j] === a) {
-              counter++
-              console.log(counter)
-            }
+    for (let j = 0; j < numbers.length; j++ ){
+      let compareNumber = numbers[j]
+      // console.log( "第" + j + "j=" + compareNumber)
+      
+      if (indexNumber === compareNumber) {
+      counter++
+      // console.log(counter)
+      }
 
     } 
 
     if(counter === 1 ){
-      return a
+      return indexNumber
     } 
   
   }
-
-  
-
-
-
   
 }
 
 
-// // console.log(findDifferent([ 8 , 3, 3, 3, 3, 3, 3, 3,])); // 印出 8
-// console.log(findDifferent([3, 1, 1, 1, 1, 1 , " "])); // 印出 3
-// // console.log(findDifferent([ 2, 2, 2, 2,4])) ;// 印出 4
+console.log(findDifferent([ 8 , 3, 3, 3, 3, 3, 3, 3,])); // 印出 8
+console.log(findDifferent([3, 1, 1, 1, 1, 1 ])); // 印出 3
+console.log(findDifferent([ 2, 2, 2, 2,4])) ;// 印出 4
 
 
