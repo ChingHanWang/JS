058d9e1727975b48e5e69358097633b922d5641a
shelly <shelly808002121231@gmail.com>
2023-07-03 01:12:31 +0800

    Sugar06_add command

--- sugar/sugar.js
+++ sugar/sugar.js
@@ -2,19 +2,21 @@
 // 程式語言：JavaScript
 // 題目：找出在數字陣列裡跟其它元素不一樣的值
 
+//宣告一個counter，起始為0，利用2個for 迴圈將元素與陣列中其他元素判斷是否相等，如果相等counter加1，
+//當迴圈執行比對結束後，在判斷counter等於1為陣列中與別人不一樣的元素
+
 function findDifferent(numbers) {
   for (let i = 0; i < numbers.length; i++ ) { 
     let counter = 0  
-    let indexNumber = numbers[i]
-    // console.log( "第" + i + "i=" + indexNumber )
-    
+
+    let indexNumber = numbers[i]    
     for (let j = 0; j < numbers.length; j++ ){
       let compareNumber = numbers[j]
-      // console.log( "第" + j + "j=" + compareNumber)
+
       
       if (indexNumber === compareNumber) {
       counter++
-      // console.log(counter)
+
       }
 
     } 
@@ -22,7 +24,7 @@ function findDifferent(numbers) {
     if(counter === 1 ){
       return indexNumber
     } 
-  
+
   }
   
 }
