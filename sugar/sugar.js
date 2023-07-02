8fd2bd1ee7c86331fe924e58b3055b98440c0f4e
shelly <shelly808002121231@gmail.com>
2023-07-03 00:49:56 +0800

    Sugar06_Use for & if  to be a solution

--- sugar/sugar.js
+++ sugar/sugar.js
@@ -2,17 +2,44 @@
 // 程式語言：JavaScript
 // 題目：找出在數字陣列裡跟其它元素不一樣的值
 
-function findDifferent(numbers, excludedValue) {
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] !== excludedValue) {
-      return excludedValue;
-    }
+function findDifferent(numbers) {
+  for (let i = 0; i < numbers.length; i++ ) { 
+    let  counter = 0
+    
+    
+          let a = numbers[i]
+          console.log( "第" + i + "i=" + a )
+
+
+          for (let j = 0; j < numbers.length; j++ ){
+            b = numbers[j]
+
+            console.log( "第" + j + "j=" + b)
+            
+          
+            if (numbers[j] === a) {
+              counter++
+              console.log(counter)
+            }
+
+    } 
+
+    if(counter === 1 ){
+      return a
+    } 
+  
   }
-  return null; // 如果找不到不同的數字，可以返回null或其他適當的值
+
+  
+
+
+
+  
 }
 
-console.log(findDifferent([1, 1, 1, 1, 3, 1, 1, 1])); // 印出 3
-//  console.log(findDifferent([2, 2, 2, 4, 2, 2])) // 印出 4
-//  console.log(findDifferent([8, 3, 3, 3, 3, 3, 3, 3])) // 印出 8
+
+// // console.log(findDifferent([ 8 , 3, 3, 3, 3, 3, 3, 3,])); // 印出 8
+// console.log(findDifferent([3, 1, 1, 1, 1, 1 , " "])); // 印出 3
+// // console.log(findDifferent([ 2, 2, 2, 2,4])) ;// 印出 4
 
 
