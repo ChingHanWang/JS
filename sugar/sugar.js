1c59ede704c6843f740306c8964eecc18bc4c183
shelly <shelly808002121231@gmail.com>
2023-07-03 14:42:30 +0800

    Sugar07_add sugar07 quiz

--- sugar/sugar.js
+++ sugar/sugar.js
@@ -1,31 +1,11 @@
-// 編號：CANDY-006
+// 編號：CANDY-007
 // 程式語言：JavaScript
-// 題目：找出在數字陣列裡跟其它元素不一樣的值
+// 題目：在某個數字陣列裡，可能藏有某個不合群的奇數或偶數，試著找出它！
 
-//宣告一個counter，起始為0，利用2個for 迴圈將元素與陣列中其他元素判斷是否相等，如果相等counter加1，
-//當迴圈執行比對結束後，在判斷counter等於1為陣列中與別人不一樣的元素
-
-function findDifferent(numbers) {
-  for (let i = 0; i < numbers.length; i++ ) { 
-    let counter = 0  
-
-    let indexNumber = numbers[i]
-    for (let j = 0; j < numbers.length; j++ ){
-      let compareNumber = numbers[j]
-      if (indexNumber === compareNumber) {
-        counter++
-      }
-    } 
-    if(counter === 1 ){
-      return indexNumber
-    } 
-  }
-  
+function findSomeDifferent(numbers) {
+  // 實作寫在這裡
 }
 
-
-console.log(findDifferent([ 8 , 3, 3, 3, 3, 3, 3, 3,])); // 印出 8
-console.log(findDifferent([3, 1, 1, 1, 1, 1 ])); // 印出 3
-console.log(findDifferent([ 2, 2, 2, 2,4])) ;// 印出 4
-
+console.log(findSomeDifferent([2, 4, 0, 100, 4, 11, 2602, 36])) // 印出 11
+console.log(findSomeDifferent([160, 3, 1719, 19, 11, 13, -21])) // 印出 160
 
