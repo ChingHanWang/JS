d5cd372a4ed711b19d0b33bf7466c8b8b22d67ca
shelly <shelly808002121231@gmail.com>
2023-07-11 23:53:29 +0800

    sugar07 was done

--- sugar/sugar.js
+++ sugar/sugar.js
@@ -3,7 +3,29 @@
 // 題目：在某個數字陣列裡，可能藏有某個不合群的奇數或偶數，試著找出它！
 
 function findSomeDifferent(numbers) {
-  // 實作寫在這裡
+  let even = []
+  let odd = []
+  
+  numbers.forEach(element => {
+      if ( element % 2 == 0){
+      even.push(element)
+      }
+      else {
+        odd.push(element)
+      }
+      
+    });
+
+    if(even.length < odd.length ){
+      return even.toString()
+    }
+    else{
+      return odd.toString()
+    }
+
+
+  
+  // 將numbers使用foreach拆分成兩個陣列，一個為奇數，一個為偶數，在進行元素長度比對，回傳短的陣列
 }
 
 console.log(findSomeDifferent([2, 4, 0, 100, 4, 11, 2602, 36])) // 印出 11
