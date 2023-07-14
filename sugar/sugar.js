1b80acf385e8dc3c652c297ea2c6a874ad6900bd
shelly <shelly808002121231@gmail.com>
2023-07-14 22:44:36 +0800

    add Sugar09 Quiz

--- sugar/sugar.js
+++ sugar/sugar.js
@@ -1,13 +1,11 @@
-// 編號：CANDY-008
+// 編號：CANDY-009
 // 程式語言：JavaScript
-// 題目：傳入一字串，計算得分最高的字
-//      英文字母 a 得 1 分、b 得 2 分、c 得 3 分，以此類推。
-//      所有傳入的字都是小寫。
+// 題目：移除網址中的錨點（Anchor）
 
-function highestScoreWord(input) {
+function removeAnchor(url) {
   // 實作寫在這裡
 }
 
-console.log(highestScoreWord("lorem ipsum dolor sit amet")) // 印出 ipsum
-console.log(highestScoreWord("heyn i need a rubygem up to build this")) // 印出 rubygem
-console.log(highestScoreWord("in time machine there are some bugs")) // 印出 there
+console.log(removeAnchor("5xruby.tw")) // 印出 5xruby.tw
+console.log(removeAnchor("5xruby.tw/#about")) // 印出 5xruby.tw/
+console.log(removeAnchor("5xruby.tw/courses/?page=1#about")) // 印出 5xruby.tw/courses/?page=1
