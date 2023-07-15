c7ed0361ae18aab40e70fbdcd55413c454973238
shelly <shelly808002121231@gmail.com>
2023-07-15 10:41:04 +0800

    練習replace()使用方法

--- practice.js
+++ practice.js
@@ -1,31 +1,10 @@
-// list => [1, 4, 2, 3, 8, 7, 10, 12]
-// 挑出陣列裡的奇數，乘以 3 之後，計算總和
+function replaceCharacters(str, char1, char2) {
+ return  str.replace()
 
-
-let list =  [1, 4, 2, 3, 8, 7, 10, 12]
-let sum= 0
-for( i = 0 ; i < list.length ; i++ ){
-  
-  if(list[i]% 2 !== 0 ){
-    sum += list[i]*3
-    // sum = sum + list[i]
-
-  }
 
 }
-console.log( sum )
 
 
+console.log(replaceCharacters("Hello World", "l", "*"));
+// 输出： "He**o Wor*d"
 
-
-
-// const list = [1, 4, 2, 3, 8, 7, 10, 12];
-
-// const result = list
-//   .filter((el) => el % 2 == 1)
-//   .map((el) => el * 3)
-//   .reduce((a, b) => a + b);
-// console.log(result);
-
-// list = [1, 4, 2, 3, 8, 7, 10, 12];
-// 挑出陣列裡的奇數，乘以 3 之後，計算總和
